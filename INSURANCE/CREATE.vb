01b3a27491cde90f4a935c003a5129978dd0fb05
Mbaje Rwaka Eric <60686069+MBAJERWAKAERIC@users.noreply.github.com>
2021-12-30 17:45:25 +0300

    Create CREATE.vb

---
+++ INSURANCE/CREATE.vb
@@ -0,0 +1,44 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class CREATE
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Try
+            Dim myConnection As String = "datasource=localhost;port=3306;username=root;"
+            Dim mycon As MySqlConnection = New MySqlConnection(myConnection)
+            Dim SelectCommand As MySqlCommand = New MySqlCommand("insert into  insurance.login(username,password) values('" & TextBox1.Text & "','" & TextBox2.Text & "') ", mycon)
+            Dim myReader As MySqlDataReader
+            mycon.Open()
+            myReader = SelectCommand.ExecuteReader()
+            MessageBox.Show("Data has been saved")
+
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+           
+            mycon.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+
+        Me.Hide()
+        Form1.Show()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Me.Hide()
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Hide()
+        Form1.Show()
+    End Sub
+
+    Private Sub Label3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label3.Click
+
+    End Sub
+End Class
