d67cc3a0cdf082ed189550225f86637f1821530e
Mbaje Rwaka Eric <60686069+MBAJERWAKAERIC@users.noreply.github.com>
2021-12-30 17:45:45 +0300

    Create employees.vb

---
+++ INSURANCE/employees.vb
@@ -0,0 +1,158 @@
+﻿Imports MySql.Data.MySqlClient
+Imports System.IO
+Public Class employees
+
+    Private Sub Button8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button8.Click
+        Me.Hide()
+        HOME.Show()
+    End Sub
+
+    Private Sub Button9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button9.Click
+        Me.Hide()
+        Form1.Show()
+    End Sub
+
+    Private Sub Button10_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button10.Click
+        Me.Hide()
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Try
+            Dim myConnection As String = "datasource=localhost;port=3306;username=root;"
+            Dim mycon As MySqlConnection = New MySqlConnection(myConnection)
+            Dim SelectCommand As MySqlCommand = New MySqlCommand("insert into  insurance.employee(member_id,firstname,lastname,gender,date,job_title,department,salary_scale,insurance_status,original_address,contact) values('" & TextBox1.Text & "','" & TextBox2.Text & "','" & TextBox3.Text & "','" & ComboBox1.Text & "','" & DateTimePicker1.Text & "','" & TextBox4.Text & "','" & TextBox5.Text & "','" & TextBox6.Text & "','" & ComboBox2.Text & "','" & TextBox7.Text & "','" & TextBox8.Text & "') ", mycon)
+            Dim myReader As MySqlDataReader
+            mycon.Open()
+            myReader = SelectCommand.ExecuteReader()
+            MessageBox.Show("Data has been saved")
+
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+            TextBox3.Text = ""
+            ComboBox1.Text = ""
+            DateTimePicker1.Text = ""
+            TextBox4.Text = ""
+            TextBox5.Text = ""
+            TextBox6.Text = ""
+            ComboBox2.Text = ""
+            TextBox7.Text = ""
+            TextBox8.Text = ""
+
+            mycon.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button11.Click
+        Try
+            Dim constring As String = "datasource=localhost;port=3306;username=root;"
+            Dim conDatabase As MySqlConnection = New MySqlConnection(constring)
+            Dim cmdDatabase As MySqlCommand = New MySqlCommand("Select * from  insurance.employee ", conDatabase)
+            Dim sda As MySqlDataAdapter = New MySqlDataAdapter()
+            sda.SelectCommand = cmdDatabase
+            Dim dbDataset As DataTable = New DataTable()
+            sda.Fill(dbDataset)
+            Dim bSource As BindingSource = New BindingSource()
+            bSource.DataSource = dbDataset
+            DataGridView1.DataSource = bSource
+            sda.Update(dbDataset)
+            conDatabase.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        ComboBox1.Text = ""
+        DateTimePicker1.Text = ""
+        TextBox4.Text = ""
+        TextBox5.Text = ""
+        TextBox6.Text = ""
+        ComboBox2.Text = ""
+        TextBox7.Text = ""
+        TextBox8.Text = ""
+
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        Try
+            Dim myConnection As String = "datasource=localhost;port=3306;username=root;"
+            Dim mycon As MySqlConnection = New MySqlConnection(myConnection)
+            Dim SelectCommand As MySqlCommand = New MySqlCommand("delete from insurance.employee  where member_id='" & TextBox1.Text & "' ", mycon)
+            Dim myReader As MySqlDataReader
+
+            mycon.Open()
+            myReader = SelectCommand.ExecuteReader()
+            MessageBox.Show("Data has been deleted")
+
+            mycon.Close()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+
+        End Try
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Try
+            Dim constring As String = "datasource=localhost;port=3306;username=root;"
+            Dim conDatabase As MySqlConnection = New MySqlConnection(constring)
+            Dim cmdDatabase As MySqlCommand = New MySqlCommand("Select * from  insurance.employee where member_id='" + TextBox1.Text + "' ;", conDatabase)
+            Dim sda As MySqlDataAdapter = New MySqlDataAdapter()
+            sda.SelectCommand = cmdDatabase
+            Dim dbDataset As DataTable = New DataTable()
+            sda.Fill(dbDataset)
+            Dim bSource As BindingSource = New BindingSource()
+            bSource.DataSource = dbDataset
+            DataGridView1.DataSource = bSource
+            sda.Update(dbDataset)
+            conDatabase.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        Try
+            Dim myConnection As String = "datasource=localhost;port=3306;username=root;"
+            Dim mycon As MySqlConnection = New MySqlConnection(myConnection)
+            Dim SelectCommand As MySqlCommand = New MySqlCommand("update  insurance.employee set firstname='" & TextBox2.Text & "',lastname='" & TextBox3.Text & "',gender='" & ComboBox1.Text & "',date='" & DateTimePicker1.Text & "',job_title='" & TextBox4.Text & "',department='" & TextBox5.Text & "',salary_scale='" & TextBox6.Text & "',insurance_status='" & ComboBox2.Text & "',contact='" & TextBox8.Text & "' where member_id='" & TextBox1.Text & "' ", mycon)
+            Dim myReader As MySqlDataReader
+
+            mycon.Open()
+
+
+            myReader = SelectCommand.ExecuteReader()
+            MessageBox.Show("Data has been updated")
+
+
+            mycon.Close()
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+
+        End Try
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim picture As New OpenFileDialog
+        Dim ms As New IO.MemoryStream
+        picture.Filter = "Choose Image(*.JPG;*.PNG;*.GIF)|*.jpg;*.png;*.gif"
+
+        If picture.ShowDialog = Windows.Forms.DialogResult.OK Then
+            PictureBox1.Image = Image.FromFile(picture.FileName)
+
+            PictureBox1.Image.Save(ms, PictureBox1.Image.RawFormat)
+        End If
+    End Sub
+End Class
